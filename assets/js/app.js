f1e234a0043677448421b1dc274640806653f262
Mahmoud <86891587+xM4hmoud@users.noreply.github.com>
2021-08-02 12:31:59 +0200

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,5 +0,0 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/Portfolio/sw.js')
-      .then(reg => console.log('service worker registered'))
-      .catch(err => console.log('service worker not registered', err));
-  }
